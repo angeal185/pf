1d54696b9b64433976e778c6fe8d8cbab187e02e
ben <beneaves01@hotmail.com>
2019-10-09 22:52:29 +0800

    init

--- store/js/store.js
+++ store/js/store.js
@@ -43,11 +43,15 @@ $k(document).ready(function() {
   let srchform = $k('#search_mini_form'),
   satinfo = $k('.tm_headerlinkmenu'),
   cright = $k('.footer_bottom_inner address'),
-  ftLeft = $k('#footer');
+  ftLeft = $k('#footer'),
+  nletter = $k('#emailSignupForm').parent('div').clone(true, true);
+
+  $k('#emailSignupForm').parent('div').remove()
+
   $k('.header_top').append(srchform.clone(true));
   srchform.remove();
 
-  $k('.breadcrumbs').eq(0).after(satinfo.clone(true));
+  $k('.breadcrumbs').eq(0).append(satinfo.clone(true));
   satinfo.remove();
 
   $k('.footer_custom').append($k('#header-img').clone().addClass('footer_logo'), cright.clone())
@@ -58,4 +62,26 @@ $k(document).ready(function() {
   ftLeft.remove()
 
   $k('.page .wrapper br').remove()
+
+  $k('.col-main-inner').append(nletter.removeAttr('style').addClass('.newsletter'))
+
 });
+
+var signUpTpl = h('div.newsletter',
+  h('form#emailSignupForm',
+    h('div.block-content.row',
+      h('div.col-md-6',
+
+      ),h('div.col-md-6',
+        h('input')
+      )
+    )
+  )
+)
+
+  <div class="form-subscribe-header">
+      <label for="newsletter" style="font-weight:bold;">Take 10% Off Your Order!&nbsp;</label><img class="hasToolTip" src="/images/assets/tooltip-icon.png" data-hasqtip="0">
+      <div style="display:none;">Sign Up for Our Weekly Emails. Be the first to know about Deals &amp; Clearance! Unsubscribe Anytime. Your privacy is Important to us.</div>
+      <input type="text" name="email" id="footer_email" title="Sign up for our newsletter" class="text field fb-email input-text required-entry validate-email" placeholder="Enter Your Email Address">
+      <button type="submit" title="Sign Up" class="button" id="emailSignupBtn">SIGN UP</button>
+  </div>
