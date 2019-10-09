81ccfe7fed1e077c5bec2c357f4b6d91699271a4
ben <beneaves01@hotmail.com>
2019-10-10 05:37:50 +0800

    init

--- store/js/store.js
+++ store/js/store.js
@@ -1,6 +1,33 @@
-//store
-var wow = new WOW({delay: '1s',duration: '1s'});
-var signUpTpl = h('div.newsletter',
+const cl = console.log,
+ce = console.error,
+jp = JSON.parse,
+js = JSON.stringify,
+LS = localStorage,
+SS = sessionStorage,
+ls = {
+  get: function(i){
+    return jp(LS.getItem(i))
+  },
+  set: function(i, e){
+    LS.setItem(i, js(e))
+  },
+  del: function(i){
+    return LS.removeItem(i)
+  }
+},
+ss = {
+  get: function(i){
+    return jp(SS.getItem(i))
+  },
+  set: function(i, e){
+    SS.setItem(i, js(e))
+  },
+  del: function(i){
+    return SS.removeItem(i)
+  }
+},
+wow = new WOW({delay: '1s',duration: '1s'}),
+signUpTpl = h('div.newsletter',
   h('form#emailSignupForm',
     h('div.row.inline-flex',
       h('div.col-md-6',
@@ -20,7 +47,14 @@ var signUpTpl = h('div.newsletter',
       )
     )
   )
+),
+recentTpl = h('div#recentviews.block',
+  h('div.block-title.border-color1',
+    h('h2.sidebar-header', 'Recently viewed')
+  ),
+  h('div#recentitems.media-list')
 )
+
 window.scrollTo({
   top: 0,
   behavior: 'smooth'
@@ -85,4 +119,110 @@ $k(document).ready(function() {
 
   $k('.col-main-inner').append(signUpTpl)
 
+});
+
+let cnf = {
+  store: ['recent','saved']
+}
+
+for (let i = 0; i < cnf.store.length; i++) {
+  if(!ls.get(cnf.store[i])){
+    ls.set(cnf.store[i], [])
+  }
+}
+
+function checkrecent(i){
+  let recent = ls.get('recent');
+  cl(i)
+
+  if(i[0] === 'product'){
+    //store recent
+    let img = $k('.img-wrap').eq(0).find('img').attr('src'),
+    url;
+
+    if(!img || img === ''){
+      img = $K('.product-image img').attr('src');
+    }
+
+
+    let obj = {
+      img: img,
+      title: i[2],
+      id: i[1],
+      price: $k('.price.actual-product-price').text(),
+      stars: $k('#review_anchor .review-stars i.fa-star').length,
+      save: $k('.price-box div').eq(2).find('span').text()
+    }
+
+    if(recent.length > 0){
+      recent = recent.filter(function(o) {
+        return o.id !== obj.id
+      })
+      recent.unshift(obj);
+    } else {
+      recent.push(obj);
+    }
+
+    if(recent.length > 5){
+      recent = recent.slice(0, 5)
+    }
+
+    return ls.set('recent', recent)
+
+  } else if(i[0] === 'category' || i[0] === 'topsextoys'){
+
+    $k('#desktop-sidebar').prepend(recentTpl);
+
+    if(recent.length === 0){
+      return $k('#recentitems').text('no recently viewed items')
+    }
+
+    for (let i = 0; i < recent.length; i++) {
+      url = ['', 'product', recent[i].id, recent[i].title].join('/');
+      $k('#recentitems').append(
+        h('div.media.recentitem',
+          h('div.media-left',
+            h('a', {
+              href: url
+              },
+              h('img.media-object', {
+                src:recent[i].img
+              })
+            )
+          ),
+          h('div.media-body',
+            h('h5', recent[i].title.replace(/-/g, ' ')),
+            h('h6', 'price: ', h('span', recent[i].price)),
+            h('h6', 'save: ', h('span',  recent[i].save)),
+            h('div.review-stars.recentstars'),
+            h('div.viewrecent',
+              h('a.viewrecentlnk',{
+                href: url
+              }, 'view')
+            )
+          )
+        )
+      )
+      let len = recent[i].stars;
+      for (let x = 0; x < len; x++) {
+        $k('.recentstars').eq(i).append(h('i.fa.fa-star'))
+      }
+      for (let x = 0; x < (5 - len); x++) {
+        $k('.recentstars').eq(i).append(h('i.fa.fa-star-o'))
+      }
+    }
+
+  } else {
+    //dont show
+    return;
+  }
+
+}
+
+checkrecent(location.pathname.slice(1).split('/'))
+
+$k(document).ready(function() {
+  $.getJSON('https://cdn.jsdelivr.net/gh/angeal185/pf/store/data/tags.json', function(data, textStatus) {
+      /*optional stuff to do after success */
+  });
 });
